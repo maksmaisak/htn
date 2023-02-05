23f0a166a69f5e68d9bfe0d21411972b1bab6f02
Maks <10959136+maksmaisak@users.noreply.github.com>
2023-02-05 14:20:47 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,8 +5,12 @@ let newURL = "/docs.html#/";
 let indexURL = "index.html";
 let frontURL = "front.html";
 if (location && location.href) {
-    if (!location.href.endsWith(oldURL) && location.href.includes(oldURL) && !location.href.includes(newURL)) {
-        location.replace(location.href.replace(oldURL, newURL));
+    if (location.href.includes(oldURL) && !location.href.includes(newURL)) {
+        if (location.href.endsWith(oldURL)) {
+            location.replace(location.href.replace(oldURL, "/" + frontURL));
+        } else {
+            location.replace(location.href.replace(oldURL, newURL));
+        }
     } else if (location.href.includes(indexURL)) {
         location.replace(location.href.replace(indexURL, frontURL));
     } else {
