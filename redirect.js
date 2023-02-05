dfba654755f1fa5bde77294ec1285f4681d267d3
Maks <10959136+maksmaisak@users.noreply.github.com>
2023-02-05 14:13:25 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,7 +5,7 @@ let newURL = "/docs.html#/";
 let indexURL = "index.html";
 let frontURL = "front.html";
 if (location && location.href) {
-    if (location.href.includes(oldURL) && !location.href.includes(newURL)) {
+    if (!location.href.endsWith(oldURL) && location.href.includes(oldURL) && !location.href.includes(newURL)) {
         location.replace(location.href.replace(oldURL, newURL));
     } else if (location.href.includes(indexURL)) {
         location.replace(location.href.replace(indexURL, frontURL));
